e90cfbc532188d90c9492fd384c1ed51b37ae42f
Samuel Path <samuel.path@gmail.com>
2017-09-22 16:59:11 +0200

    up to #8

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
+import './css/style.css';
 
+import App from './components/App';
 import StorePicker from './components/StorePicker';
+import Header from './components/Header';
 
-render(<StorePicker/>, document.querySelector('#main'));
+render(<App/>, document.querySelector('#main'));
