9f676ccc0a100e5a4fe743ca9caec67de988cd28
Samuel Path <samuel.path@gmail.com>
2017-09-27 14:48:25 +0200

    changes from 26th SEP

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,11 +2,17 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import Fish from './Fish';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
   constructor() {
     super();
+
     this.addFish = this.addFish.bind(this);
+    this.loadSamples = this.loadSamples.bind(this);
+    this.addToOrder = this.addToOrder.bind(this);
+    
     this.state = {
       fishes: {},
       order: {}
@@ -20,14 +26,32 @@ class App extends React.Component {
     this.setState({ fishes });
   }
 
+  loadSamples() {
+    this.setState({ fishes: sampleFishes });
+  }
+
+  addToOrder(key) {
+    console.log(`key: ${key}`)
+    const order = {...this.state.order};
+    order[key] = order[key] + 1 || 1;
+    this.setState({ order });
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
+          <ul className="list-of-fishes">
+            {
+              Object
+                .keys(this.state.fishes)
+                .map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />)
+            }
+          </ul>
         </div>
-        <Order />
-        <Inventory addFish={this.addFish} />
+        <Order fishes={this.state.fishes} order={this.state.order} />
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
       </div>
     )
   }
--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { formatPrice } from '../helpers';
 
 class Order extends React.Component {
   render() {
+    const { fishes, order } = this.props;
+
+    const obj = Object
+      .keys(order)
+      .map(key => fishes[key].price * order[key]);
+    console.log("order: " + JSON.stringify(order));
+    console.log("fishes: " + JSON.stringify(fishes));
+    console.log("obj: " + obj);
+
     return (
-      <h2>Order</h2>
+      <div>
+        <h2>Order</h2>
+        <ul className="order">
+          {
+            Object
+              .keys(order)
+              .map(key => <p>{fishes[key].name}, {order[key]} lbs, {formatPrice(fishes[key].price * order[key])}</p>)
+          }
+        </ul>
+        <h2>Total: </h2>
+      </div>
     )
   }
 }
