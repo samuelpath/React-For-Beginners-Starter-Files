ea77a31e371f1721e95a1483fe3b628373cd10ae
Samuel Path <samuel.path@gmail.com>
2017-10-04 13:14:08 +0200

    03-OCT changes

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyD4xTOU0CvjyZrH27em7EgIGuIGfP9t1fo",
+  authDomain: "catch-of-the-day-a5c80.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-a5c80.firebaseio.com"
+});
+
+export default base;
